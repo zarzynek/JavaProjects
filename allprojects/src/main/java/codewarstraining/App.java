66d03f994452b5f33ab58cf1f997538360dcde26
zarzynek <adam.will.warsaw@gmail.com>
2021-07-05 14:08:10 +0100

    Clean up the packages

---
+++ allprojects/src/main/java/codewarstraining/App.java
@@ -0,0 +1,19 @@
+package codewarstraining;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author adamw
+ */
+public class App {
+    public static void main(String[] args)
+    {
+      
+    }
+    
+}
